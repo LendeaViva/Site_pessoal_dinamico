676c433443914748dc3a6c4275ddfaad0bb795ad
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-01-08 10:37:01 +0000

    Add project files.

---
+++ Site_v3_dinamico/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Site_v3_dinamico.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Site_v3_dinamico/wwwroot/js/meujs.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+s
+function mostrar() {
+    var x = document.getElementById("mostrarDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
