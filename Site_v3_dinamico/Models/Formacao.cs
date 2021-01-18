34c2575bc5ac626d9bfa0a439ab393a4ef071873
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-01-18 18:41:22 +0000

    Criado modelo Formacao

---
+++ Site_v3_dinamico/Models/Formacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site_v3_dinamico.Models
+{
+    public class Formacao
+    {
+        public int Id { get; set; }
+        public string nomeInstituicao { get; set; }
+
+       
+        public string dataIniciodataFim { get; set; }
+        public string nomeCurso { get; set; }
+        public string conteudosCurso { get; set; }
+    }
+}
