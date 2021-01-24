5b4365d8eaaeccb33547092d516d9d5abc2fe1ce
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-01-24 17:42:57 +0000

    Scaffolding Competencias

---
+++ Site_v3_dinamico/Controllers/CompetenciasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Site_v3_dinamico.Data;
+using Site_v3_dinamico.Models;
+
+namespace Site_v3_dinamico.Controllers
+{
+    public class CompetenciasController : Controller
+    {
+        private readonly SiteDinamicoBdContext _context;
+
+        public CompetenciasController(SiteDinamicoBdContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Competencias
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Competencias.ToListAsync());
+        }
+
+        // GET: Competencias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var competencias = await _context.Competencias
+                .FirstOrDefaultAsync(m => m.CompetenciasId == id);
+            if (competencias == null)
+            {
+                return NotFound();
+            }
+
+            return View(competencias);
+        }
+
+        // GET: Competencias/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Competencias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CompetenciasId,nomeComp,descricaoComp,nomeLinguagem,nivelComp")] Competencias competencias)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(competencias);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(competencias);
+        }
+
+        // GET: Competencias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var competencias = await _context.Competencias.FindAsync(id);
+            if (competencias == null)
+            {
+                return NotFound();
+            }
+            return View(competencias);
+        }
+
+        // POST: Competencias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CompetenciasId,nomeComp,descricaoComp,nomeLinguagem,nivelComp")] Competencias competencias)
+        {
+            if (id != competencias.CompetenciasId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(competencias);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompetenciasExists(competencias.CompetenciasId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(competencias);
+        }
+
+        // GET: Competencias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var competencias = await _context.Competencias
+                .FirstOrDefaultAsync(m => m.CompetenciasId == id);
+            if (competencias == null)
+            {
+                return NotFound();
+            }
+
+            return View(competencias);
+        }
+
+        // POST: Competencias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var competencias = await _context.Competencias.FindAsync(id);
+            _context.Competencias.Remove(competencias);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompetenciasExists(int id)
+        {
+            return _context.Competencias.Any(e => e.CompetenciasId == id);
+        }
+    }
+}
