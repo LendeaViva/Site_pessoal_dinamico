727b6d7a5ac79e45bbf71a633e5c331a7f9237aa
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-01-26 09:29:55 +0000

    Criado SeedData

---
+++ Site_v3_dinamico/Data/SeedData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site_v3_dinamico.Data
+{
+    public class SeedData
+    {
+    }
+}
