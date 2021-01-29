4708c62d7e477b7758f899bea3154cf2183c6a06
LendeaViva <73750123+LendeaViva@users.noreply.github.com>
2021-01-29 10:10:19 +0000

    Alterações nas propriedades de exp_profissional

--- Site_v3_dinamico/Models/Exp_profissional.cs
+++ Site_v3_dinamico/Models/Exp_profissional.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,13 +10,26 @@ namespace Site_v3_dinamico.Models
     {
         public int Exp_ProfissionalId { get; set; }
 
+        [Required(ErrorMessage = "Insira o nome da empresa.")]
+        [StringLength(200, MinimumLength = 4, ErrorMessage = "O nome deve ter pelo menos 4 caracteres e não deve exceder os 200 caracteres.")]
+        [Display(Name = "Nome da empresa")]
         public string nomeEmpresa { get; set; }
-        public int dataInicio { get; set; }
 
-        public int dataFim { get; set; }
+        [Display(Name = "Data de início")]
+        public DateTime dataInicio { get; set; }
+
+        [Display(Name = "Data de fim")]
+        public DateTime dataFim { get; set; }
+
+        [Required(ErrorMessage = "Insira a função/cargo desempenhado")]
+        [Display(Name = "Função/Cargo")]
         public string funcao { get; set; }
-
+       
+        [Display(Name = "Descrição da função/cargo")]
         public string descricaoFuncao { get; set; }
+
+        [Display(Name = "Logótipo da empresa")]
+        public byte[] logotipo { get; set; }
 
 
     }
